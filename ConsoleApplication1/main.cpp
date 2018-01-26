95aa7f2f31950273c3b9f4721662b2cdba4355e0
jc <1573533796@qq.com>
2018-01-26 10:16:49 +0800

    添加项目文件。

---
+++ ConsoleApplication1/main.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    printf("hello from ConsoleApplication1!\n");
+ 	cout << "this is a test" << endl;
+
+    return 0;
+}
